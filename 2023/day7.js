fb653f316739303cba04ac102905dbbc244e4867
Drew <dangerworm@gmail.com>
2023-12-08 22:29:23 +0000

    Day 7 works! Finally!

--- 2023/day7.js
+++ 2023/day7.js
@@ -1,36 +1,43 @@
 const fs = require('fs')
 
 const CARD_ORDER = 'AKQJT98765432'
+const CARD_ORDER_WITH_JOKERS = 'AKQT98765432J'
+
 const FIVE_OF_A_KIND = 1
 const FOUR_OF_A_KIND = 2
 const FULL_HOUSE = 3
 const THREE_OF_A_KIND = 4
 const TWO_PAIR = 5
-const HIGH_CARD = 6
+const ONE_PAIR = 6
+const HIGH_CARD = 7
 
 const getType = (hand) => {
   const maxCount = Math.max(...Object.values(hand))
   const minCount = Math.min(...Object.values(hand))
 
-  if (maxCount === 5) {
-    return FIVE_OF_A_KIND
-  }
-  if (maxCount === 4) {
-    return FOUR_OF_A_KIND
-  }
-  if (maxCount === 3 && minCount === 2) {
-    return FULL_HOUSE
-  }
-  if (maxCount === 3) {
-    return THREE_OF_A_KIND
-  }
-  if (maxCount === 2 && Object.values(hand).length === 3) {
-    return TWO_PAIR
-  }
+  if (maxCount === 5) return FIVE_OF_A_KIND
+  if (maxCount === 4) return FOUR_OF_A_KIND
+  if (maxCount === 3 && minCount === 2) return FULL_HOUSE
+  if (maxCount === 3) return THREE_OF_A_KIND
+  if (maxCount === 2 && Object.values(hand).length === 3) return TWO_PAIR
+  if (maxCount === 2) return ONE_PAIR
   return HIGH_CARD
 }
 
-const sortPlayers = (player1, player2) => {
+const getTypeWithJokers = (hand) => {
+  const type = getType(hand)
+
+  if (type === FOUR_OF_A_KIND && hand['J']) return FIVE_OF_A_KIND
+  if (type === FULL_HOUSE && hand['J']) return FIVE_OF_A_KIND
+  if (type === THREE_OF_A_KIND && hand['J']) return FOUR_OF_A_KIND
+  if (type === TWO_PAIR && hand['J'] === 2) return FOUR_OF_A_KIND
+  if (type === TWO_PAIR && hand['J'] === 1) return FULL_HOUSE
+  if (type === ONE_PAIR && hand['J']) return THREE_OF_A_KIND
+  if (type === HIGH_CARD && hand['J'] === 1) return ONE_PAIR
+  return type
+}
+
+const sortPlayers = (player1, player2, cardOrder) => {
   if (player1.type > player2.type) {
     return -1
   }
@@ -42,10 +49,10 @@ const sortPlayers = (player1, player2) => {
       const player1Card = player1.line[i]
       const player2Card = player2.line[i]
 
-      if (CARD_ORDER.indexOf(player1Card) > CARD_ORDER.indexOf(player2Card)) {
+      if (cardOrder.indexOf(player1Card) > cardOrder.indexOf(player2Card)) {
         return -1
       }
-      else if (CARD_ORDER.indexOf(player1Card) < CARD_ORDER.indexOf(player2Card)) {
+      else if (cardOrder.indexOf(player1Card) < cardOrder.indexOf(player2Card)) {
         return 1
       }
     }
@@ -53,39 +60,50 @@ const sortPlayers = (player1, player2) => {
   return 0
 }
 
-let sum = 0
+const parse = (fileName) =>
+  fs.readFileSync(fileName, 'utf8')
+    .split(/\r?\n/)
+    .map((line) => {
+      const [hand, bid] = line.split(' ')
+      return {
+        line: line,
+        bid: Number(bid),
+        hand: hand
+          .split('')
+          .reduce((dict, card) => {
+            if (dict[card]) {
+              dict[card]++
+            } else {
+              dict[card] = 1
+            }
+            return dict
+          }, {})
+      }
+    })
 
-const data = fs
-  .readFileSync('day7.txt', 'utf8')
-  .split(/\r?\n/)
-  .map((line) => {
-    const [hand, bid] = line.split(' ')
-    return {
-      line: line,
-      bid: Number(bid),
-      hand: hand
-        .split('')
-        .reduce((dict, card) => {
-          if (dict[card]) {
-            dict[card]++
-          } else {
-            dict[card] = 1
-          }
-          return dict
-        }, {})
-    }
-  })
-  .map((player) => ({
-    ...player,
-    type: getType(player.hand)
-  }))
-  .sort(sortPlayers)
+const part1 = (data) =>
+  data
+    .map((player) => ({
+      ...player,
+      type: getType(player.hand)
+    }))
+    .sort((p1, p2) => sortPlayers(p1, p2, CARD_ORDER))
 
-data
-  .forEach((player, index) => {
-    console.log(player, index + 1, sum + player.bid * (index + 1))
-    sum += player.bid * (index + 1)
-  })
+const part2 = (data) =>
+  data
+    .map((player) => ({
+      ...player,
+      type: getTypeWithJokers(player.hand)
+    }))
+    .sort((p1, p2) => sortPlayers(p1, p2, CARD_ORDER_WITH_JOKERS))
 
-console.log(data)
-console.log(sum)
+const getSum = (players) =>
+  players
+    .reduce((sum, player, index) =>
+      sum + player.bid * (index + 1)
+      , 0)
+
+const data = parse('day7.txt')
+
+console.log("Part 1:", getSum(part1(data)))
+console.log("Part 2:", getSum(part2(data)))
